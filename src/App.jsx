cdbcd94feab8402acff7dac7a9d538ea7d1172cd
Abacus-Arturo <arturo@abacusbrands.com>
2026-08-19 11:20:11 -0700

    Padding bottom panel 0

--- src/App.jsx
+++ src/App.jsx
@@ -288,7 +288,7 @@ export default function App() {
         flexDirection: 'row',
         paddingTop: '0.75rem',
         paddingBottom: '0.75rem',
-        paddingLeft: '0.75rem',
+        paddingLeft: '0rem',
         paddingRight: '0.75rem',
         gap: '0rem',
         boxSizing: 'border-box',
