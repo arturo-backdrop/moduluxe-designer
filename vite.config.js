f781195691e453254d904ecab4d35774d600f44f
arturo-backdrop <arturo.m@backdrop.com>
2026-08-20 16:33:50 -0700

    vite.config: support assetsDir from deploy.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import { DEPLOY } from './deploy.config.js';
 export default defineConfig({
   plugins: [react()],
   base: DEPLOY.base,
+  ...(DEPLOY.assetsDir ? { build: { assetsDir: DEPLOY.assetsDir } } : {}),
 });
